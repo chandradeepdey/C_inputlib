968bf7626ba4be80fc9b4e79d2845f2b1735ba34
Chandradeep Dey <chandradeepdey@posteo.net>
2019-07-15 17:09:43 +0530

    Add get_dynstring() for dynamically allocated string input

--- inputlib.c
+++ inputlib.c
@@ -5,8 +5,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <stdint.h>
 #include <limits.h>
+#define INIT_SIZE 10
 #include "inputlib.h"
+
+/* gets a string upto the next newline from a given stream
+ *
+ * Parameters
+ * 1) stream is the stream from which input is taken. if stream
+ * is NULL, stdin is used.
+ *
+ * Return
+ * a pointer to the new string. In case there are errors allocating
+ * memory, NULL pointer is returned.
+ * If the return value is not NULL, it should be freed using free()
+ * before exiting the program.
+ */
+char* get_dynstring(FILE *stream)
+{
+        if (stream == NULL)
+                stream = stdin;
+
+        char *ret, *temp = NULL;
+        size_t size = INIT_SIZE;
+        ret = malloc(size);
+
+        if (ret != NULL) {
+                int ch;
+                size_t i = 0;
+                while ((ch = getc(stream)), ch != '\n' && ch != EOF) {
+                        if (i == size - 1) {
+                                if (size <= SIZE_MAX / 2)
+                                        temp = realloc(ret, size * 2);
+                                if (temp != NULL) {
+                                        size *= 2;
+                                        ret = temp;
+                                } else {
+                                        free(ret);
+                                        ret = NULL;
+                                }
+                        }
+                        if (ret == NULL)
+                                break;
+                        ret[i] = ch;
+                        i++;
+                }
+                if (ret != NULL)
+                        ret[i] = '\0';
+        }
+
+        return ret;
+}
 
 /* gets a string from a given stream
  *
@@ -53,8 +103,7 @@ char* get_string(char *s, int n, FILE *stream)
         return ret;
 }
 
-/*
- * Any input until the next newline character is consumed by this function
+/* Any input until the next newline character is consumed by this function
  *
  * Inputs
  * 1) stream is the stream on which the operation is to be performed. stdin
@@ -70,8 +119,7 @@ void clear_line(FILE *stream)
                 ;
 }
 
-/*
- * buffer used by functions below this point
+/* buffer used by functions below this point
  */
 #define STRSIZE 500
 static char input[STRSIZE];
