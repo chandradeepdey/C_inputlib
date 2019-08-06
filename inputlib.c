1a1cdbf0b02efb7ea599289b45a30c674eec2fde
Chandradeep Dey <chandradeepdey@posteo.net>
2019-08-07 00:26:56 +0530

    Add a proper getline function

--- inputlib.c
+++ inputlib.c
@@ -9,6 +9,99 @@
 #include <limits.h>
 #include "inputlib.h"
 
+/* an implementation of the POSIX getline() function.
+ * this implementation is cross platform, but it is certainly slower than
+ * a built-in library function. If you have such a function available, then you
+ * should avoid using this.
+ * further, this function does not follow the specs of POSIX. In particular,
+ * it does not set errno to anything, and it does not use an ssize_t type as
+ * its return value.
+ *
+ * Inputs
+ * 1) *lineptr must point to an area allocated using the malloc()
+ *    family of functions, or it must be NULL. if *lineptr is NULL
+ *    the function starts by allocating its own memory and storing
+ *    the address in *lineptr. Otherwise, the area pointed to by
+ *    *lineptr is used by the function, growing it using realloc()
+ *    as required. this can also change the value of *lineptr.
+ * 2) n is a pointer to the size of the area associated with *lineptr.
+ *    if *lineptr was not NULL, incorrect value of *n is undefined behaviour.
+ *    *n is modified by the function as lineptr is grown.
+ * 3) stream should be an input stream or an update stream or a NULL pointer.
+ *    if it is a NULL pointer, the input is automatically taken from stdin.
+ *    Otherwise, stream is the stream from which input is to be taken.
+ *
+ * Returns
+ * 0
+ *  in this case *lineptr and *n are left unmodified.
+ *  this occurs if
+ *   1) lineptr was NULL
+ *   2) n was NULL
+ *   3) *lineptr was NULL and the function failed to initially allocate
+ *      an one byte storage for the empty string.
+ *
+ * nonzero
+ *   the return value is the number of bytes among the *n bytes that were
+ *   stored into by this function.
+ *
+ * Notes
+ * *lineptr must be freed using free() to avoid memory leaks.
+ *
+ * in the event that an attempt to grow *lineptr fails, any characters that
+ * were not stored into the current *lineptr are left on the stream. Otherwise
+ * the function consumes all input till the first newline character that
+ * appears. The newline character is discarded and not stored in the storage
+ * area pointed to by *lineptr.
+ *
+ * the strings formed by this function are always null terminated. However,
+ * the standard library functions may fail to operate properly if the input
+ * itself somehow contained a null character.
+ * A test for this case can be written as
+ * strlen(*lineptr) == one less than the return value of this function, given
+ * it was nonzero.
+ */
+size_t cust_getline(char **lineptr, size_t *n, FILE *stream)
+{
+        if (lineptr == NULL || n == NULL)
+                return 0;
+        if (stream == NULL)
+                stream = stdin;
+        if (feof(stream) || ferror(stream))
+                return 0;
+
+        if (*lineptr == NULL) {
+                *lineptr = malloc(1);
+                if (*lineptr)
+                        *n = 1;
+                else
+                        return 0;
+        }
+
+        size_t i = 0;
+        int ch;
+        while ((ch = getc(stream)), ch != '\n' && ch != EOF) {
+                if (i == *n - 1) {
+                        if (*n <= SIZE_MAX / 2) {
+                                char *temp = realloc(*lineptr, *n * 2);
+                                if (temp != NULL) {
+                                        *n *= 2;
+                                        *lineptr = temp;
+                                } else {
+                                        ungetc(ch, stream);
+                                        break;
+                                }
+                        } else {
+                                ungetc(ch, stream);
+                                break;
+                        }
+                }
+                (*lineptr)[i++] = ch;
+        }
+        (*lineptr)[i++] = '\0';
+
+        return i;
+}
+
 /* gets a string upto the next newline from a given stream
  *
  * Parameters
--- inputlib.h
+++ inputlib.h
@@ -2,10 +2,14 @@
 #define INPUTLIB_H_
 
 char* get_dynstring(FILE *stream);
+size_t cust_getline(char **lineptr, size_t *n, FILE *stream);
+
 int get_int(FILE *stream);
 long int get_long(FILE *stream);
-double get_double(FILE *stream);
+
 unsigned int get_unsigned(FILE *stream);
 unsigned long long int get_unsigned_long_long(FILE *stream);
+
+double get_double(FILE *stream);
 
 #endif
